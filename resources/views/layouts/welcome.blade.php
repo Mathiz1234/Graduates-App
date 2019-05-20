d4a6ea1c60fb504f4b494af7e4d265bbf9ec94cb
Mateusz Sutor <mateusz.0167@gmail.com>
2019-05-20 20:41:53 +0200

    Change main site view

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -22,12 +22,56 @@
 
 @endif
 
-<div class="jumbotron mt-3">
-        <h1 class="display-4">System absolwentów! - W BUDOWIE </h1>
-        <p class="lead">Przeszukuj spis absolwentów szkoły Staszica! Teraz to bardzo proste!</p>
-        <hr class="my-4">
-        <p>Aby w pełni moc korzystać z usługi, zaloguj się.</p>
-        <a class="btn btn-primary btn-lg" href="{{route('graduates.index')}}" role="button">Przeszukaj teraz!</a>
-</div>
+<section class="d-none d-md-block mt-2">
+        <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="{{ asset('img/1-slider.png') }}" class="d-block w-100" alt="...">
+              <div class="carousel-caption-new d-none d-md-block">
+                <h5>{{ __('List of graduates')}}</h5>
+                <p>{{ __ ('Search the graduates of your school!')}}</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="{{ asset('img/2-slider.png') }}" class="d-block w-100" alt="...">
+              <div class="carousel-caption-new d-none d-md-block">
+                <h5>{{ __('Easy access')}}</h5>
+                <p>{{ __('Thanks to the cloud you can use the system all over the world.')}}</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img src="{{ asset('img/3-slider.png') }}" class="d-block w-100" alt="...">
+              <div class="carousel-caption-new d-none d-md-block">
+                <h5>{{ __('Save, edit, delete ...')}}</h5>
+                <p>{{ __('Log into the system to get more options')}}</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">@lang('pagination.previous')</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">@lang('pagination.next')</span>
+          </a>
+        </div>
+</section>
+
+<section class="card text-center my-2">
+        <div class="card-header">
+                {{ __('general.graduate') }}
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{ __('Search the graduates database now!')}}</h5>
+          <p class="card-text">{{ __('To fully use the service, log in.')}}</p>
+          <a href="{{route('graduates.index')}}" class="btn btn-primary">{{ __('Click now!')}}</a>
+        </div>
+</section>
 
 @endsection
